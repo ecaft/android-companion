d8bbbac390e10ac2c8b5e09e5e9ee88a443a210b
lauralindy <laura.f.lin@gmail.com>
2018-01-09 20:26:51 -0500

    camera WIP

--- app/src/main/java/edu/ashleyxue/ecaft/NotesFragment.java
+++ app/src/main/java/edu/ashleyxue/ecaft/NotesFragment.java
@@ -1,19 +1,36 @@
 package edu.ashleyxue.ecaft;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by robotbf on 12/28/17.
@@ -29,10 +46,17 @@ public class NotesFragment extends Fragment{
     private StorageReference storageRef = FirebaseApplication
             .getStorageRef();
 
+    private List<String> myList;  // String list that contains file paths to images
+    private GridView gridview;
+    private String mCurrentPhotoPath;
+
+    protected AbsListView mListView;
+    protected TextView mEmptyTextView;
+    protected ProgressDialog mLoadingProgressDialog;
 
     public NotesFragment() {
-        // Empty constructor
-    }
+        }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.notes_fragment, container, false);
@@ -47,8 +71,9 @@ public class NotesFragment extends Fragment{
         companyLocation.setText("Table " + companyTable);
 
         Button notesButton = (Button) v.findViewById(R.id.notesButton);
+        Button cameraButton = (Button) v.findViewById(R.id.cameraButton);
 
-        notesButton.setOnClickListener(new View.OnClickListener() {
+       /* notesButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.d("Notes Page", "Button Being Clicked");
                 NotesFragment noteFrag = new NotesFragment();
@@ -58,7 +83,15 @@ public class NotesFragment extends Fragment{
                 Log.d("Notes Page", "CALLING NEW FRAGMENT");
                 transaction.commit();
             }
+        }); */
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("Notes Page", "CAMERA Button Being Clicked");
+                Intent myIntent = new Intent(getActivity(), CameraActivity.class);
+                getActivity().startActivity(myIntent);
+            }
         });
+
         companyLogo = (ImageView) v.findViewById(R.id.company_details_logo);
 
         StorageReference path = storageRef.child("logos/" +
@@ -73,5 +106,4 @@ public class NotesFragment extends Fragment{
 
         return v;
     }
-
 }
